267508ebd4dcaa2d5beb3d508042b8f71f347b4b
jbabiak <j.babiak@hotmail.com>
2016-05-03 11:48:26 -0400

    werrrking

--- ranking.js
+++ ranking.js
@@ -9,7 +9,18 @@
 (function ($) {
   Drupal.behaviors.field_ranking_colorpicker = {
     attach: function(context) {
-    
+      $('select[name*="rank-select"').change(function(){
+        // start by setting everything to enabled
+        $('select[name*="rank-select"] option').attr('disabled',false);
+        // loop each select and set the selected value to disabled in all other selects
+        $('select[name*="rank-select"]').each(function(){
+          var $this = $(this);
+          $('select[name*="rank-select"]').not($this).find('option').each(function(){
+            if($(this).attr('value') == $this.val())
+              $(this).attr('disabled',true);
+          });
+        });
+      });
     }
   }
 })(jQuery);
