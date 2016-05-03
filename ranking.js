ec12ad9bc85280bf88620ee54d6d2fc3458f2cf9
jbabiak <j.babiak@hotmail.com>
2016-05-03 11:18:57 -0400

    new changes

--- ranking.js
+++ ranking.js
@@ -9,17 +9,7 @@
 (function ($) {
   Drupal.behaviors.field_ranking_colorpicker = {
     attach: function(context) {
-      $(".edit-field-ranking-colorpicker").live("focus", function(event) {
-        var edit_field = this;
-        var picker = $(this).closest('div').parent().find(".field-ranking-colorpicker");
-        
-        // Hide all color pickers except this one.
-        $(".field-ranking-colorpicker").hide();
-        $(picker).show();
-        $.farbtastic(picker, function(color) {
-          edit_field.value = color;
-        }).setColor(edit_field.value);
-      });
+    
     }
   }
 })(jQuery);
