93934a77ee96c3a9fd4a84a99ba327f371390d04
jbabiak <j.babiak@hotmail.com>
2016-05-04 12:49:51 -0400

    i think this is good

--- ranking.js
+++ ranking.js
@@ -6,20 +6,23 @@
 /**
  * Provides a farbtastic colorpicker for the fancier widget.
  */
+
+
 (function ($) {
   Drupal.behaviors.field_ranking_colorpicker = {
     attach: function(context) {
-      $(".edit-field-ranking-colorpicker").live("focus", function(event) {
-        var edit_field = this;
-        var picker = $(this).closest('div').parent().find(".field-ranking-colorpicker");
-        
-        // Hide all color pickers except this one.
-        $(".field-ranking-colorpicker").hide();
-        $(picker).show();
-        $.farbtastic(picker, function(color) {
-          edit_field.value = color;
-        }).setColor(edit_field.value);
-      });
+                $('select').change(function(){
+                    // start by setting everything to enabled
+                        $('#rank-select option').attr('disabled',false);
+                   // loop each select and set the selected value to disabled in all other selects
+                        $('select').each(function(){
+                           var $this = $(this);
+                  $('select').not($this).find('option').each(function(){
+                              if($(this).attr('value') == $this.val() && $(this).attr('value') != 0)
+                                 $(this).attr('disabled',true);
+                              });
+                          });
+                 });
     }
   }
 })(jQuery);
