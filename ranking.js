6130888b6c71b311087a9ae799f25f57b6627e42
jbabiak <j.babiak@hotmail.com>
2016-05-04 14:26:52 -0400

    boom

--- ranking.js
+++ ranking.js
@@ -1,26 +1,19 @@
-/**
- * @file
- * Javascript for Field Example.
- */
-
-/**
- * Provides a farbtastic colorpicker for the fancier widget.
- */
 (function ($) {
-  Drupal.behaviors.field_ranking_colorpicker = {
-    attach: function(context) {
-      $('select[name*="rank-select"').change(function(){
-        // start by setting everything to enabled
-        $('select[name*="rank-select"] option').attr('disabled',false);
-        // loop each select and set the selected value to disabled in all other selects
-        $('select[name*="rank-select"]').each(function(){
-          var $this = $(this);
-          $('select[name*="rank-select"]').not($this).find('option').each(function(){
-            if($(this).attr('value') == $this.val())
-              $(this).attr('disabled',true);
-          });
-        });
-      });
+    Drupal.behaviors.fieldranking = {
+        attach: function(context) {
+            $('select[id*="rank-select"]').change(function(){
+                // start by setting everything to enabled
+                $('select[id*="rank-select"]').find('option').attr('disabled',false);
+                // loop each select and set the selected value to disabled in all other selects
+                $('select[id*="rank-select"]').each(function(){
+                    var $this = $(this);
+                    $('select[id*="rank-select"]').not($this).find('option').each(function(){
+                        if($(this).attr('value') == $this.val() && $(this).val() != 0) {
+                            $(this).attr('disabled',true);
+                        }
+                    });
+                });
+            });
+        }
     }
-  }
 })(jQuery);
